fabe2a676f02abc6f079c3b5550b2978d9fcf152
abdomagdy1012 <62938057+abdomagdy1012@users.noreply.github.com>
2026-05-06 14:34:36 +0300

    Update Cart and CartContext

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,54 +1,80 @@
-import axios from "axios";
-import { createContext } from "react";
+import { useContext, useEffect, useState } from 'react'
+import style from './Cart.module.css'
+import { CartContext } from '../../Context/CartContext';
+import { BallTriangle } from 'react-loader-spinner'
 
+export default function Cart(){
+    let{getLoggedCart , deleteProductCart ,updateProductCart} = useContext(CartContext); 
 
-export let CartContext = createContext();
+    const [cartDetails, setCartDetails] = useState(null)
 
-
-
-export default function CartContextProvider(props){
-    let headers = {
-        token:localStorage.getItem('userToken')
-    }
-
-    function addToCart(id){
-        return axios.post(`https://route-ecommerce.onrender.com/api/v1/cart`, 
-        {
-            productId:id
+    async function getCart(){
+        let {data} = setCartDetails({
+  data: {
+    totalCartPrice: 149,
+    products: [
+      {
+        product: {
+          id: "1",
+          title: "woman shawl",
+          imageCover: "https://ecommerce.routemisr.com/Route-Academy-products/1680403397402-cover.jpeg"
         },
-    {
-        headers:headers
-    }).then((response) => response)
-    .catch((err)=> err);
+        price: 149,
+        count: 1
+      }
+    ]
+  }
+});
+        setCartDetails(data);
+    }
+    async function removeProductCart(id){
+        let {data} = await deleteProductCart(id)
+        setCartDetails(data);
+    }
+    async function updateitemCart(id , count){
+        let {data} = await updateProductCart(id ,count)
+        setCartDetails(data);
     }
 
-    function getLoggedCart(){
-        return axios.get(`https://route-ecommerce.onrender.com/api/v1/cart`, 
-    {
-        headers:headers
-    }).then((response) => response)
-    .catch((err)=> err);
-    }
-    
-    function deleteProductCart(productId){
-        return axios.delete(`https://route-ecommerce.onrender.com/api/v1/cart/${productId}`, 
-    {
-        headers:headers
-    }).then((response) => response)
-    .catch((err)=> err);
-    }
-    function updateProductCart(productId , count){
-        return axios.put(`https://route-ecommerce.onrender.com/api/v1/cart/${productId}`, 
-    {
-        count:count
-    }, 
-    {
-        headers:headers
-    }).then((response) => response)
-    .catch((err)=> err);
-    }
 
-    return <CartContext.Provider value={{addToCart , getLoggedCart,deleteProductCart,updateProductCart}}>
-        {props.children}
-    </CartContext.Provider>
+    useEffect(()=>{
+        getCart();
+    },[])
+    return <> { cartDetails?  <div className="w-75 mt-4 px-4 py-3 mx-auto bg-main-light">
+    <h2 className=''>Shop Cart:</h2>
+    <h4 className=' text-main mb-4'>Total card Price: {cartDetails.data.totalCartPrice} EGP</h4>
+    {cartDetails.data.products.map((product) => <div key={product.product.id} className='row border-bottom py-2 px-2'>
+        <div className="col-md-1">
+            <img className='w-100' src={product.product.imageCover} alt="" />
+        </div>
+        <div className="col-md-11">
+            <div className='d-flex justify-content-between align-items-center'>
+                <div>
+                    <h6 > {product.product.title}</h6>
+                    <h6 className='text-main'>Price: {product.price} EGP</h6>
+                </div>
+                <div>
+                    <button onClick={()=> updateitemCart(product.product.id , product.count+1)} className=' brdr-main p-1'>+</button>
+                    <span className='mx-2'>{product.count}</span>
+                    <button onClick={()=> updateitemCart(product.product.id , product.count-1)} className='brdr-main p-1'>-</button>
+                </div>
+                
+            </div>
+            <button onClick={()=> removeProductCart(product.product.id)} className='btn p-0 '><i className='text-danger fas fa-trash-can'></i>  Remove</button>
+        </div>
+    </div>)}
+
+    </div> : <div className='d-flex justify-content-center w-100'>
+    <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+        />
+        </div>}
+</>
 }
